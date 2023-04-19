664c5cac013e45eb919dbb7d695d88abc48dd052
Alfredone212 <aa.rayner@outlook.com>
2023-04-19 08:16:02 +0000

    .

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import Navbar from '@/components/Navbar'
 
 export default function Home() {
   return (
-    <main>
+    <main className='bg-blue-500'>
       <Navbar/>
       <h1 className='flex text-5xl justify-center'>WOW THIS HAS BEEN CENTERED ALFIE LOOK AT ME WAHOOO</h1>
 
